6abce0e143a1785cb6566dbf6d4c2db691e65b9e
rotemgoren <34604067+rotemgoren@users.noreply.github.com>
2019-01-24 13:37:02 +0200

    Update segNet.py

--- segNet.py
+++ segNet.py
@@ -19,7 +19,6 @@ from keras.layers.normalization import BatchNormalization
 from collections import OrderedDict
 from keras.utils.np_utils import to_categorical
 from keras import backend as K
-import tensorflow as tf
 
 import os
 file_path = os.path.dirname( os.path.abspath(__file__) )
@@ -127,47 +126,47 @@ def VGGSegnet( n_classes ,  input_height=416, input_width=608 ):
 
 
 
-def camvid_colors_dataset():
-    camvid_colors = OrderedDict([
-        ("Animal", np.array([64, 128, 64], dtype=np.uint8)),
-        ("Archway", np.array([192, 0, 128], dtype=np.uint8)),
-        ("Bicyclist", np.array([0, 128, 192], dtype=np.uint8)),
-        ("Bridge", np.array([0, 128, 64], dtype=np.uint8)),
-        ("Building", np.array([128, 0, 0], dtype=np.uint8)),
-        ("Car", np.array([64, 0, 128], dtype=np.uint8)),
-        ("CartLuggagePram", np.array([64, 0, 192], dtype=np.uint8)),
-        ("Child", np.array([192, 128, 64], dtype=np.uint8)),
-        ("Column_Pole", np.array([192, 192, 128], dtype=np.uint8)),
-        ("Fence", np.array([64, 64, 128], dtype=np.uint8)),
-        ("LaneMkgsDriv", np.array([128, 0, 192], dtype=np.uint8)),
-        ("LaneMkgsNonDriv", np.array([192, 0, 64], dtype=np.uint8)),
-        ("Misc_Text", np.array([128, 128, 64], dtype=np.uint8)),
-        ("MotorcycleScooter", np.array([192, 0, 192], dtype=np.uint8)),
-        ("OtherMoving", np.array([128, 64, 64], dtype=np.uint8)),
-        ("ParkingBlock", np.array([64, 192, 128], dtype=np.uint8)),
-        ("Pedestrian", np.array([64, 64, 0], dtype=np.uint8)),
-        ("Road", np.array([128, 64, 128], dtype=np.uint8)),
-        ("RoadShoulder", np.array([128, 128, 192], dtype=np.uint8)),
-        ("Sidewalk", np.array([0, 0, 192], dtype=np.uint8)),
-        ("SignSymbol", np.array([192, 128, 128], dtype=np.uint8)),
-        ("Sky", np.array([128, 128, 128], dtype=np.uint8)),
-        ("SUVPickupTruck", np.array([64, 128, 192], dtype=np.uint8)),
-        ("TrafficCone", np.array([0, 0, 64], dtype=np.uint8)),
-        ("TrafficLight", np.array([0, 64, 64], dtype=np.uint8)),
-        ("Train", np.array([192, 64, 128], dtype=np.uint8)),
-        ("Tree", np.array([128, 128, 0], dtype=np.uint8)),
-        ("Truck_Bus", np.array([192, 128, 192], dtype=np.uint8)),
-        ("Tunnel", np.array([64, 0, 64], dtype=np.uint8)),
-        ("VegetationMisc", np.array([192, 192, 0], dtype=np.uint8)),
-        ("Wall", np.array([64, 192, 0], dtype=np.uint8)),
-        ("Void", np.array([0, 0, 0], dtype=np.uint8))
-    ])
-    return camvid_colors
+
+camvid_colors = OrderedDict([
+    ("Animal", np.array([64, 128, 64], dtype=np.uint8)),
+    ("Archway", np.array([192, 0, 128], dtype=np.uint8)),
+    ("Bicyclist", np.array([0, 128, 192], dtype=np.uint8)),
+    ("Bridge", np.array([0, 128, 64], dtype=np.uint8)),
+    ("Building", np.array([128, 0, 0], dtype=np.uint8)),
+    ("Car", np.array([64, 0, 128], dtype=np.uint8)),
+    ("CartLuggagePram", np.array([64, 0, 192], dtype=np.uint8)),
+    ("Child", np.array([192, 128, 64], dtype=np.uint8)),
+    ("Column_Pole", np.array([192, 192, 128], dtype=np.uint8)),
+    ("Fence", np.array([64, 64, 128], dtype=np.uint8)),
+    ("LaneMkgsDriv", np.array([128, 0, 192], dtype=np.uint8)),
+    ("LaneMkgsNonDriv", np.array([192, 0, 64], dtype=np.uint8)),
+    ("Misc_Text", np.array([128, 128, 64], dtype=np.uint8)),
+    ("MotorcycleScooter", np.array([192, 0, 192], dtype=np.uint8)),
+    ("OtherMoving", np.array([128, 64, 64], dtype=np.uint8)),
+    ("ParkingBlock", np.array([64, 192, 128], dtype=np.uint8)),
+    ("Pedestrian", np.array([64, 64, 0], dtype=np.uint8)),
+    ("Road", np.array([128, 64, 128], dtype=np.uint8)),
+    ("RoadShoulder", np.array([128, 128, 192], dtype=np.uint8)),
+    ("Sidewalk", np.array([0, 0, 192], dtype=np.uint8)),
+    ("SignSymbol", np.array([192, 128, 128], dtype=np.uint8)),
+    ("Sky", np.array([128, 128, 128], dtype=np.uint8)),
+    ("SUVPickupTruck", np.array([64, 128, 192], dtype=np.uint8)),
+    ("TrafficCone", np.array([0, 0, 64], dtype=np.uint8)),
+    ("TrafficLight", np.array([0, 64, 64], dtype=np.uint8)),
+    ("Train", np.array([192, 64, 128], dtype=np.uint8)),
+    ("Tree", np.array([128, 128, 0], dtype=np.uint8)),
+    ("Truck_Bus", np.array([192, 128, 192], dtype=np.uint8)),
+    ("Tunnel", np.array([64, 0, 64], dtype=np.uint8)),
+    ("VegetationMisc", np.array([192, 192, 0], dtype=np.uint8)),
+    ("Wall", np.array([64, 192, 0], dtype=np.uint8)),
+    ("Void", np.array([0, 0, 0], dtype=np.uint8))
+])
+
 
 
 def convert_label_to_grayscale(im):
     out = (np.ones(im.shape[:2]) * 0).astype(np.uint8)
-    for gray_val, (label, rgb) in enumerate(camvid_colors_dataset().items()):
+    for gray_val, (label, rgb) in enumerate(camvid_colors.items()):
         match_pxls = np.where((im == np.asarray(rgb)).sum(-1) == 3)
         out[match_pxls] = gray_val
     #assert (out != 255).all(), "rounding errors or missing classes in camvid_colors"
@@ -175,7 +174,7 @@ def convert_label_to_grayscale(im):
 
 def label_to_color(im):
     out = (np.ones((im.shape[0],im.shape[1],3)) * 255).astype(np.uint8)
-    for gray_val, (label, rgb) in enumerate(camvid_colors_dataset().items()):
+    for gray_val, (label, rgb) in enumerate(camvid_colors.items()):
         match_pxls=np.where((im == gray_val))
         out[match_pxls]=rgb
     
@@ -188,7 +187,7 @@ def prepar_data(batch_file):
     if (os.path.isfile('labeled_images_{}.npy'.format(batch_file))):
         y=np.load('labeled_images_{}.npy'.format(batch_file))
     else:
-        PATH = 'D:/segmentaion/*.png' #os.path.join(file_path,'Data\\Labeled\\','*.png')
+        PATH = os.path.join(file_path,'Data\\Labeled\\','*.png')
         y=[]
         count=0
         i=0
@@ -217,8 +216,8 @@ def prepar_data(batch_file):
         x=np.load('input_images_{}.npy'.format(batch_file))
     else:
         x=[]   
-        PATH="D:/segmentaion/data/*.png"
-        #PATH = os.path.join(PATH,"Raw/","*.png")
+        PATH="C:/Users/owner/Desktop/segmentaion/data/"
+        PATH = os.path.join(PATH,"Raw/","*.png")
         count=0
         i=0
         for image in glob.glob(PATH):
@@ -240,7 +239,7 @@ def prepar_data(batch_file):
     
 def train_model(x=[],y=[]):   
 
-        val_ratio=0.85
+        val_ratio=0.8
         
         perm=np.random.permutation(x.shape[0])
         x=x[perm,:,:,:]
@@ -281,7 +280,7 @@ def train_model(x=[],y=[]):
         
      
                 
-        model_gpu.fit(x_train,y_train,epochs=10,shuffle=False,batch_size=1,validation_data=(x_valid,y_valid),callbacks=[])      
+        model_gpu.fit(x_train,y_train,epochs=50,shuffle=False,batch_size=3,validation_data=(x_valid,y_valid),callbacks=[])      
             
 
 
@@ -296,11 +295,8 @@ def test_model(x_test):
     if os.path.isfile(weights_file_name):
         model.load_weights(weights_file_name)
     
-    x_test1=np.expand_dims(x_test[0], axis=0)
-    
-    
-    with tf.device('/gpu:0'):
-        x_predict=model.predict(x_test1,batch_size=1)
+    x_test1=np.expand_dims(x_test[50], axis=0)
+    x_predict=model.predict(x_test1,batch_size=1)
 
     x_predict=np.reshape(x_predict[0],(WIDTH,HIGHT,32))
 
@@ -319,15 +315,25 @@ def one_hot_it(labels):
             x[i,j,labels[i][j]]=1
     return x
 
-
-x_train,y_train=prepar_data(0)
-
-for _ in range(20):
-
-        train_model(x_train,y_train)
-
-x_predict=test_model(x_train)
-
-
-plt.figure()
-plt.imshow(x_predict)
+if __name__=='__main__':
+    x_train,y_train=prepar_data(0)
+    '''
+    for _ in range(5):
+    
+            train_model(x_train,y_train)
+    '''
+    
+    x_predict=test_model(x_train)
+    
+    
+    plt.figure()
+    plt.imshow(x_predict)
+    plt.figure()
+    y=y_train[50,:,:]
+    y=np.reshape(y,(WIDTH,HIGHT,32))
+    
+    y=np.argmax(y,axis=2)
+    y=label_to_color(y)
+    plt.imshow(y)
+    
+    #prepar_data()    
